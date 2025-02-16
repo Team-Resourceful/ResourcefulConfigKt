99fe2a0e23f3c0f64ac8e8fe07fd6acdb89a443c
Gravy Boat <gravy@thatgravyboat.tech>
2025-02-16 02:34:49 -0330

    Use delegate provider instead

--- common/src/main/kotlin/com/teamresourceful/resourcefulconfigkt/api/builders/EntriesBuilder.kt
+++ common/src/main/kotlin/com/teamresourceful/resourcefulconfigkt/api/builders/EntriesBuilder.kt
@@ -6,63 +6,79 @@ import com.teamresourceful.resourcefulconfig.api.types.options.TranslatableValue
 import com.teamresourceful.resourcefulconfigkt.KotlinConfigEntry
 import com.teamresourceful.resourcefulconfigkt.api.EntryDelegate
 import java.util.LinkedHashMap
+import kotlin.reflect.KProperty
 
 open class EntriesBuilder {
 
     internal val entries: LinkedHashMap<String, ResourcefulConfigEntry> = LinkedHashMap()
 
-    internal fun <T, B : TypeBuilder> entry(
-        id: String,
-        type: EntryType,
-        builderFactory: (String) -> B,
-        builderPopulator: (B) -> Unit,
-        value: T,
-    ): EntryDelegate<T> {
-        val property = EntryDelegate(value, value)
-        var data = builderFactory(id).apply(builderPopulator).toEntryData()
+    fun byte(value: Byte, builder: NumberBuilder<Byte>.() -> Unit = {}) = Entry(null, EntryType.BYTE, ::NumberBuilder, builder, value)
+    fun byte(id: String, value: Byte, builder: NumberBuilder<Byte>.() -> Unit = {}) = Entry(id, EntryType.BYTE, ::NumberBuilder, builder, value)
 
-        entries[id] = KotlinConfigEntry<Any>(
-            type,
-            { property.set(it as T) },
-            { property.get() as Any },
-            data,
-            value as Any
-        )
-        return property
-    }
+    fun bytes(vararg value: Byte, builder: NumberBuilder<Byte>.() -> Unit = {}) = Entry(null, EntryType.BYTE, ::NumberBuilder, builder, byteArrayOf(*value))
+    fun bytes(id: String, vararg value: Byte, builder: NumberBuilder<Byte>.() -> Unit = {}) = Entry(id, EntryType.BYTE, ::NumberBuilder, builder, byteArrayOf(*value))
 
-    fun byte(id: String, value: Byte, builder: NumberBuilder<Byte>.() -> Unit = {}) = entry(id, EntryType.BYTE, ::NumberBuilder, builder, value)
-    fun bytes(id: String, vararg value: Byte, builder: NumberBuilder<Byte>.() -> Unit = {}) = entry(id, EntryType.BYTE, ::NumberBuilder, builder, byteArrayOf(*value))
+    fun short(value: Short, builder: NumberBuilder<Short>.() -> Unit = {}) = Entry(null, EntryType.SHORT, ::NumberBuilder, builder, value)
+    fun short(id: String, value: Short, builder: NumberBuilder<Short>.() -> Unit = {}) = Entry(id, EntryType.SHORT, ::NumberBuilder, builder, value)
 
-    fun short(id: String, value: Short, builder: NumberBuilder<Short>.() -> Unit = {}) = entry(id, EntryType.SHORT, ::NumberBuilder, builder, value)
-    fun shorts(id: String, vararg value: Short, builder: NumberBuilder<Short>.() -> Unit = {}) = entry(id, EntryType.SHORT, ::NumberBuilder, builder, shortArrayOf(*value))
+    fun shorts(vararg value: Short, builder: NumberBuilder<Short>.() -> Unit = {}) = Entry(null, EntryType.SHORT, ::NumberBuilder, builder, shortArrayOf(*value))
+    fun shorts(id: String, vararg value: Short, builder: NumberBuilder<Short>.() -> Unit = {}) = Entry(id, EntryType.SHORT, ::NumberBuilder, builder, shortArrayOf(*value))
 
-    fun int(id: String, value: Int, builder: NumberBuilder<Int>.() -> Unit = {}) = entry(id, EntryType.INTEGER, ::NumberBuilder, builder, value)
-    fun ints(id: String, vararg value: Int, builder: NumberBuilder<Int>.() -> Unit = {}) = entry(id, EntryType.INTEGER, ::NumberBuilder, builder, intArrayOf(*value))
+    fun int(value: Int, builder: NumberBuilder<Int>.() -> Unit = {}) = Entry(null, EntryType.INTEGER, ::NumberBuilder, builder, value)
+    fun int(id: String, value: Int, builder: NumberBuilder<Int>.() -> Unit = {}) = Entry(id, EntryType.INTEGER, ::NumberBuilder, builder, value)
 
-    fun long(id: String, value: Long, builder: NumberBuilder<Long>.() -> Unit = {}) = entry(id, EntryType.LONG, ::NumberBuilder, builder, value)
-    fun longs(id: String, vararg value: Long, builder: NumberBuilder<Long>.() -> Unit = {}) = entry(id, EntryType.LONG, ::NumberBuilder, builder, longArrayOf(*value))
+    fun ints(vararg value: Int, builder: NumberBuilder<Int>.() -> Unit = {}) = Entry(null, EntryType.INTEGER, ::NumberBuilder, builder, intArrayOf(*value))
+    fun ints(id: String, vararg value: Int, builder: NumberBuilder<Int>.() -> Unit = {}) = Entry(id, EntryType.INTEGER, ::NumberBuilder, builder, intArrayOf(*value))
 
-    fun float(id: String, value: Float, builder: NumberBuilder<Float>.() -> Unit = {}) = entry(id, EntryType.FLOAT, ::NumberBuilder, builder, value)
-    fun floats(id: String, vararg value: Float, builder: NumberBuilder<Float>.() -> Unit = {}) = entry(id, EntryType.FLOAT, ::NumberBuilder, builder, floatArrayOf(*value))
+    fun long(value: Long, builder: NumberBuilder<Long>.() -> Unit = {}) = Entry(null, EntryType.LONG, ::NumberBuilder, builder, value)
+    fun long(id: String, value: Long, builder: NumberBuilder<Long>.() -> Unit = {}) = Entry(id, EntryType.LONG, ::NumberBuilder, builder, value)
 
-    fun double(id: String, value: Double, builder: NumberBuilder<Double>.() -> Unit = {}) = entry(id, EntryType.DOUBLE, ::NumberBuilder, builder, value)
-    fun doubles(id: String, vararg value: Double, builder: NumberBuilder<Double>.() -> Unit = {}) = entry(id, EntryType.DOUBLE, ::NumberBuilder, builder, doubleArrayOf(*value))
+    fun longs(vararg value: Long, builder: NumberBuilder<Long>.() -> Unit = {}) = Entry(null, EntryType.LONG, ::NumberBuilder, builder, longArrayOf(*value))
+    fun longs(id: String, vararg value: Long, builder: NumberBuilder<Long>.() -> Unit = {}) = Entry(id, EntryType.LONG, ::NumberBuilder, builder, longArrayOf(*value))
 
-    fun boolean(id: String, value: Boolean, builder: TypeBuilder.() -> Unit = {}) = entry(id, EntryType.BOOLEAN, ::TypeBuilder, builder, value)
-    fun booleans(id: String, vararg value: Boolean, builder: TypeBuilder.() -> Unit = {}) = entry(id, EntryType.BOOLEAN, ::TypeBuilder, builder, booleanArrayOf(*value))
+    fun float(value: Float, builder: NumberBuilder<Float>.() -> Unit = {}) = Entry(null, EntryType.FLOAT, ::NumberBuilder, builder, value)
+    fun float(id: String, value: Float, builder: NumberBuilder<Float>.() -> Unit = {}) = Entry(id, EntryType.FLOAT, ::NumberBuilder, builder, value)
 
-    fun string(id: String, value: String, builder: StringBuilder.() -> Unit = {}) = entry(id, EntryType.STRING, ::StringBuilder, builder, value)
-    fun strings(id: String, vararg value: String, builder: StringBuilder.() -> Unit = {}) = entry(id, EntryType.STRING, ::StringBuilder, builder, value)
+    fun floats(vararg value: Float, builder: NumberBuilder<Float>.() -> Unit = {}) = Entry(null, EntryType.FLOAT, ::NumberBuilder, builder, floatArrayOf(*value))
+    fun floats(id: String, vararg value: Float, builder: NumberBuilder<Float>.() -> Unit = {}) = Entry(id, EntryType.FLOAT, ::NumberBuilder, builder, floatArrayOf(*value))
 
-    fun <T : Enum<T>> enum(id: String, value: T, builder: TypeBuilder.() -> Unit = {}): EntryDelegate<T> = entry(id, EntryType.ENUM, ::TypeBuilder, builder, value)
-    fun <T : Enum<T>> enums(id: String, vararg value: T, builder: TypeBuilder.() -> Unit = {}) = entry(id, EntryType.ENUM, ::TypeBuilder, builder, value)
+    fun double(value: Double, builder: NumberBuilder<Double>.() -> Unit = {}) = Entry(null, EntryType.DOUBLE, ::NumberBuilder, builder, value)
+    fun double(id: String, value: Double, builder: NumberBuilder<Double>.() -> Unit = {}) = Entry(id, EntryType.DOUBLE, ::NumberBuilder, builder, value)
+
+    fun doubles(vararg value: Double, builder: NumberBuilder<Double>.() -> Unit = {}) = Entry(null, EntryType.DOUBLE, ::NumberBuilder, builder, doubleArrayOf(*value))
+    fun doubles(id: String, vararg value: Double, builder: NumberBuilder<Double>.() -> Unit = {}) = Entry(id, EntryType.DOUBLE, ::NumberBuilder, builder, doubleArrayOf(*value))
+
+    fun boolean(value: Boolean, builder: TypeBuilder.() -> Unit = {}) = Entry(null, EntryType.BOOLEAN, ::TypeBuilder, builder, value)
+    fun boolean(id: String? = null, value: Boolean, builder: TypeBuilder.() -> Unit = {}) = Entry(id, EntryType.BOOLEAN, ::TypeBuilder, builder, value)
+
+    fun booleans(vararg value: Boolean, builder: TypeBuilder.() -> Unit = {}) = Entry(null, EntryType.BOOLEAN, ::TypeBuilder, builder, booleanArrayOf(*value))
+    fun booleans(id: String, vararg value: Boolean, builder: TypeBuilder.() -> Unit = {}) = Entry(id, EntryType.BOOLEAN, ::TypeBuilder, builder, booleanArrayOf(*value))
+
+    fun string(value: String, builder: StringBuilder.() -> Unit = {}) = Entry(null, EntryType.STRING, ::StringBuilder, builder, value)
+    fun string(id: String, value: String, builder: StringBuilder.() -> Unit = {}) = Entry(id, EntryType.STRING, ::StringBuilder, builder, value)
+
+    // Very hacky but sadly the varargs if its nullable makes it weird
+    fun strings(vararg value: String, builder: StringBuilder.() -> Unit = {}) = Entry(null, EntryType.STRING, ::StringBuilder, builder, value)
+    fun stringsWithId(id: String, vararg value: String, builder: StringBuilder.() -> Unit = {}) = Entry(id, EntryType.STRING, ::StringBuilder, builder, value)
+
+    fun <T : Enum<T>> enum(value: T, builder: TypeBuilder.() -> Unit = {}) = Entry(null, EntryType.ENUM, ::TypeBuilder, builder, value)
+    fun <T : Enum<T>> enum(id: String, value: T, builder: TypeBuilder.() -> Unit = {}) = Entry(id, EntryType.ENUM, ::TypeBuilder, builder, value)
+
+    fun <T : Enum<T>> enums(vararg value: T, builder: TypeBuilder.() -> Unit = {}) = Entry(null, EntryType.ENUM, ::TypeBuilder, builder, value)
+    fun <T : Enum<T>> enums(id: String, vararg value: T, builder: TypeBuilder.() -> Unit = {}) = Entry(id, EntryType.ENUM, ::TypeBuilder, builder, value)
 
     // special
-    fun key(id: String, value: Int, builder: KeyBuilder.() -> Unit = {}) = entry(id, EntryType.INTEGER, ::KeyBuilder, builder, value)
-    fun color(id: String, value: Int, builder: ColorBuilder.() -> Unit = {}) = entry(id, EntryType.INTEGER, ::ColorBuilder, builder, value)
-    fun <T : Enum<T>> select(id: String, vararg value: T, builder: SelectBuilder<T>.() -> Unit = {}) = entry(id, EntryType.ENUM, ::SelectBuilder, builder, value)
-    fun <T : Enum<T>> draggable(id: String, vararg value: T, builder: DraggableBuilder<T>.() -> Unit = {}) = entry(id, EntryType.ENUM, { DraggableBuilder(id, getEmptyArray<T>(value.javaClass)) }, builder, value)
+    fun key(value: Int, builder: KeyBuilder.() -> Unit = {}) = Entry(null, EntryType.INTEGER, ::KeyBuilder, builder, value)
+    fun key(id: String, value: Int, builder: KeyBuilder.() -> Unit = {}) = Entry(id, EntryType.INTEGER, ::KeyBuilder, builder, value)
+
+    fun color(value: Int, builder: ColorBuilder.() -> Unit = {}) = Entry(null, EntryType.INTEGER, ::ColorBuilder, builder, value)
+    fun color(id: String, value: Int, builder: ColorBuilder.() -> Unit = {}) = Entry(id, EntryType.INTEGER, ::ColorBuilder, builder, value)
+
+    fun <T : Enum<T>> select(vararg value: T, builder: SelectBuilder<T>.() -> Unit = {}) = Entry(null, EntryType.ENUM, ::SelectBuilder, builder, value)
+    fun <T : Enum<T>> select(id: String, vararg value: T, builder: SelectBuilder<T>.() -> Unit = {}) = Entry(id, EntryType.ENUM, ::SelectBuilder, builder, value)
+
+    fun <T : Enum<T>> draggable(vararg value: T, builder: DraggableBuilder<T>.() -> Unit = {}) = Entry(null, EntryType.ENUM, { DraggableBuilder(it, getEmptyArray<T>(value.javaClass)) }, builder, value)
+    fun <T : Enum<T>> draggable(id: String, vararg value: T, builder: DraggableBuilder<T>.() -> Unit = {}) = Entry(id, EntryType.ENUM, { DraggableBuilder(it, getEmptyArray<T>(value.javaClass)) }, builder, value)
 
     companion object {
 
@@ -73,5 +89,33 @@ open class EntriesBuilder {
         private fun <T> getEmptyArray(arrayClass: Class<*>): Array<T> {
             return java.lang.reflect.Array.newInstance(arrayClass.componentType, 0) as Array<T>
         }
+    }
+}
+
+class Entry<T, B : TypeBuilder> internal constructor(
+    private val id: String?,
+    private val type: EntryType,
+    private val builderFactory: (String) -> B,
+    private val builderFiller: (B) -> Unit,
+    private val value: T,
+) {
+
+    operator fun provideDelegate(builder: EntriesBuilder, prop: KProperty<*>): EntryDelegate<T> {
+        val id = id ?: prop.name
+        require(id !in builder.entries) { "Entry with id $id already exists" }
+        require(id.isNotEmpty()) { "Entry id cannot be empty" }
+        require('.' !in id) { "Entry id $id cannot contain '.'" }
+
+        val property = EntryDelegate(this.value, this.value)
+        var data = builderFactory(id).apply(builderFiller).toEntryData()
+
+        builder.entries[id] = KotlinConfigEntry<Any>(
+            type,
+            { property.set(it as T) },
+            { property.get() as Any },
+            data,
+            value as Any
+        )
+        return property
     }
 }
